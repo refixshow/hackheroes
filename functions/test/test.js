ba1fc317d68b628ea6f7c79b1904170f1edd0bd4
refixshow <adasscieszka@wp.pl>
2020-10-05 18:04:02 +0200

    asfqwefwef

--- functions/test/test.js
+++ functions/test/test.js
@@ -1,6 +1,7 @@
 exports.handler = function (event, context, callback) {
+  console.log("console log from test function")
   callback(null, {
     statusCode: 200,
-    body: "Hello, World",
+    body: "POST",
   })
 }
