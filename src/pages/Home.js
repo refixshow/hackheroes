87767d7c2afe65f42f1ece06cdf2160d1602b437
refixshow <adasscieszka@wp.pl>
2020-10-13 19:13:42 +0200

    xd

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,16 @@
 import React, { useContext } from "react"
+import { Link } from "react-router-dom"
 import AppContext from "../context/AppContext"
 
 const Home = () => {
   const { state } = useContext(AppContext)
   console.log(state)
-  return <div>Home Page</div>
+
+  return (
+    <div>
+      Home Page <Link to="/login">login</Link>
+    </div>
+  )
 }
 
 export default Home
