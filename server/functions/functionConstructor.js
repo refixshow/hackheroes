c7c4a7b39e2c28482ab3832f837c6fcf4d38a429
szymonhrehorowicz <szymhre@gmail.com>
2020-10-23 18:18:34 +0200

    xy

--- server/functions/functionConstructor.js
+++ server/functions/functionConstructor.js
@@ -17,19 +17,19 @@ const Function = (model, body) => {
         }
     };
     this.post = async () => {
-        try{
-            const {user_id, ...data} = this.body;
+        try {
+            const { user_id, ...data } = this.body;
             // Ten if jest tu dlatego, ze user_id wystepuje w metodzie POST we wszystkich funkcjach z wyjatkiem 'user'
-            if(user_id) {
+            if (user_id) {
                 data.user_id = user_id;
             }
             const newObject = new this.model(data);
             const res = await newObject.save();
             callback(null, {
                 statusCode: 201,
-                body: JSON.stringify({response: res, message: 'OK'});
-            })
-        }catch(err) {
+                body: JSON.stringify({ response: res, message: 'OK' }),
+            });
+        } catch (err) {
             callback(null, {
                 statusCode: 404,
                 body: JSON.stringify({
@@ -40,14 +40,14 @@ const Function = (model, body) => {
         }
     };
     this.patch = async () => {
-        try{
-            const {object_id, ...data} = this.body;
-            const res = await this.model.updateOne({_id: object_id}, data);
+        try {
+            const { object_id, ...data } = this.body;
+            const res = await this.model.updateOne({ _id: object_id }, data);
             callback(null, {
                 statusCode: 200,
                 body: JSON.stringify({ response: res, message: 'OK' }),
             });
-        }catch(err) {
+        } catch (err) {
             callback(null, {
                 statusCode: 404,
                 body: JSON.stringify({
@@ -58,14 +58,14 @@ const Function = (model, body) => {
         }
     };
     this.delete = async () => {
-        try{
-            const {object_id} = this.body;
-            const res = await this.model.deleteOne({_id: object_id});
+        try {
+            const { object_id } = this.body;
+            const res = await this.model.deleteOne({ _id: object_id });
             callback(null, {
                 statusCode: 200,
                 body: JSON.stringify({ response: res, message: 'OK' }),
             });
-        }catch (err) {
+        } catch (err) {
             callback(null, {
                 statusCode: 404,
                 body: JSON.stringify({
