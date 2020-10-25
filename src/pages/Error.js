43ead38c9054bf1be2b9d62c02313ab9782847e7
szymonhrehorowicz <szymhre@gmail.com>
2020-10-25 17:11:39 +0100

    Home page

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,7 +1,7 @@
-import React from "react"
+import React from "react";
 
 const Error = () => {
-  return <div>ErrorPage</div>
-}
+  return <div>ErrorPage</div>;
+};
 
-export default Error
+export default Error;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,8 +1,9 @@
 import React, { useContext, useCallback } from "react";
-import { IdentityModalContext } from "../context/IdentityModalContextProvider";
-import Button from "../components/atoms/button/Button";
+import { IdentityModalContext } from "../../context/IdentityModalContextProvider";
+import Button from "../../components/atoms/button/Button";
+import Icon from "../../components/atoms/icon/Icon";
 import style from "./Home.module.scss";
-import logo from "../assets/icons/logo.svg";
+import logo from "../../assets/icons/logo.svg";
 
 const Home = () => {
   const { setIsOpen } = useContext(IdentityModalContext);
@@ -14,9 +15,11 @@ const Home = () => {
   return (
     <main className={style.wrapper}>
       <div className={style.content}>
-        <img src={logo} className={style.logo} />
+        <Icon src={logo} className={style.logo} />
         <h1 className={style.header}>Health Companion</h1>
-        <Button onClick={handleOnClick} className={style.btn} content='Log In' />
+        <Button onClick={handleOnClick} className={style.btn}>
+          Log In
+        </Button>
       </div>
     </main>
   );
