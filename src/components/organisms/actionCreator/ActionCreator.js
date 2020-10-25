c83538c24ef1c51c80ff5cb43372d608fe4ce8f8
szymonhrehorowicz <szymhre@gmail.com>
2020-10-25 22:08:08 +0100

    Action Creator css

--- src/components/organisms/actionCreator/ActionCreator.js
+++ src/components/organisms/actionCreator/ActionCreator.js
@@ -1,29 +1,43 @@
-import React, { useCallback } from "react"
-import style from "./ActionCreator.module.scss"
+import React, { useCallback } from "react";
+import style from "./ActionCreator.module.scss";
+import Icon from "../../atoms/icon/Icon";
+import chart_icon from "../../../assets/icons/chart.svg";
+import plus_icon from "../../../assets/icons/plus.svg";
+import time_icon from "../../../assets/icons/time.svg";
 
 const ActionCreator = ({ active, setActive }) => {
   const handleChartChange = useCallback(() => {
-    setActive({ chart: true, history: false, add: false })
-  }, [setActive])
+    setActive({ chart: true, history: false, add: false });
+  }, [setActive]);
   const handleHistoryChange = useCallback(() => {
-    setActive({ chart: false, history: true, add: false })
-  }, [setActive])
+    setActive({ chart: false, history: true, add: false });
+  }, [setActive]);
   const handleAddChange = useCallback(() => {
-    setActive({ chart: false, history: false, add: true })
-  }, [setActive])
+    setActive({ chart: false, history: false, add: true });
+  }, [setActive]);
 
   return (
     <header className={style.wrapper}>
-      <span>title</span>
-      <div>
-        {!active.chart && <button onClick={handleChartChange}>---chart</button>}
-        {!active.history && (
-          <button onClick={handleHistoryChange}>---history</button>
+      <div className={style.pageTitle}>Title</div>
+      <div className={style.buttons}>
+        {!active.chart && (
+          <button onClick={handleChartChange} className={style.btn}>
+            <Icon src={chart_icon} className={style.icon} />
+          </button>
         )}
-        {!active.add && <button onClick={handleAddChange}>---add</button>}
+        {!active.history && (
+          <button onClick={handleHistoryChange} className={style.btn}>
+            <Icon src={time_icon} className={style.icon} />
+          </button>
+        )}
+        {!active.add && (
+          <button onClick={handleAddChange} className={style.btn}>
+            <Icon src={plus_icon} className={style.icon} />
+          </button>
+        )}
       </div>
     </header>
-  )
-}
+  );
+};
 
-export default ActionCreator
+export default ActionCreator;
