98bc1c7754a56a11720c393bc48f09364ba832c3
refixshow <adasscieszka@wp.pl>
2020-10-25 23:40:48 +0100

    creator

--- src/components/molecules/addBMI/AddBMI.js
+++ src/components/molecules/addBMI/AddBMI.js
@@ -1,15 +1,13 @@
-import React, { useCallback, useState, useMemo } from "react"
-import useEndPoint from "../../../hooks/useEndPoint"
-import timeSetter from "../../../helpers/time"
-import style from "./AddBMI.module.scss"
+import React, { useCallback, useState, useMemo } from "react";
+import useEndPoint from "../../../hooks/useEndPoint";
+import timeSetter from "../../../helpers/time";
+import style from "./AddBMI.module.scss";
 
 const AddBMI = () => {
-  const [weight, setWeight] = useState(0)
-  const [height, setHeight] = useState(0)
+  const [weight, setWeight] = useState(0);
+  const [height, setHeight] = useState(0);
 
-  const bmi = Math.round((weight / Math.pow(height, 2)) * 100) / 100
-
-  const longDate = useMemo(() => timeSetter({ type: "GET_LONG_DATE" }), [])
+  const longDate = useMemo(() => timeSetter({ type: "GET_LONG_DATE" }), []);
 
   const [addBMI] = useEndPoint({
     type: "POST",
@@ -17,48 +15,38 @@ const AddBMI = () => {
       queryKey: "bmi",
       endPointName: "bmi",
     },
-  })
+  });
 
   const handleSubmit = useCallback(
     (e) => {
-      e.preventDefault()
+      e.preventDefault();
       addBMI({
         user_id: "1",
         weight,
         height,
-        bmi,
+        BMI: Math.round(weight / Math.pow(height, 2)),
         date: timeSetter({ type: "MAKE_ISO_DATE", date: longDate }),
-      })
+      });
     },
-    [addBMI, longDate, weight, height, bmi]
-  )
+    [addBMI, longDate, weight, height]
+  );
 
-  const handleHeightChange = useCallback((e) => {
-    setHeight(e.target.value)
-  }, [])
-  const handleWeightChange = useCallback((e) => {
-    setWeight(e.target.value)
-  }, [])
+  const handleSys_pressure = useCallback((e) => {
+    setWeight(e.target.value);
+  }, []);
+  const handleDias_pressure = useCallback((e) => {
+    setHeight(e.target.value);
+  }, []);
 
   return (
     <div className={style.container}>
       <form onSubmit={handleSubmit} className={style.form}>
-        <input
-          type="number"
-          required
-          onChange={handleSys_pressure}
-          placeholder="Waga [kg]"
-        />
-        <input
-          type="number"
-          required
-          onChange={handleDias_pressure}
-          placeholder="Wzrost [cm]"
-        />
-        <input type="submit" className={style.btn} value="save" />
+        <input type='number' required onChange={handleSys_pressure} placeholder='Waga [kg]' />
+        <input type='number' required onChange={handleDias_pressure} placeholder='Wzrost [cm]' />
+        <input type='submit' className={style.btn} value='save' />
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default AddBMI
+export default AddBMI;
