a2898cc22b07bd89a212865c6dd258d79aac8a16
szymonhrehorowicz <szymhre@gmail.com>
2020-10-25 23:44:50 +0100

    create user

--- src/components/molecules/createUserForm/CreateUserForm.js
+++ src/components/molecules/createUserForm/CreateUserForm.js
@@ -1,26 +1,29 @@
-import React, { memo, useState, useCallback } from "react"
-import { isEqual } from "lodash"
+import React, { memo, useState, useCallback } from "react";
+import { isEqual } from "lodash";
+import style from "./CreateUserForm.module.scss";
 
 const CreateUserForm = memo(
   ({ email, name, createUser }) => {
-    const [birthDate, setBirthDate] = useState("")
+    const [birthDate, setBirthDate] = useState("");
 
     const handleSubmit = useCallback(() => {
-      createUser({ email, name, birth_date: birthDate })
-    }, [createUser, email, name, birthDate])
+      createUser({ email, name, birth_date: birthDate });
+    }, [createUser, email, name, birthDate]);
 
     const handleChange = useCallback((e) => {
-      setBirthDate(e.target.value)
-    }, [])
+      setBirthDate(e.target.value);
+    }, []);
 
     return (
-      <form onSubmit={handleSubmit}>
-        <input type="date" onChange={handleChange} required />
-        <input type="submit" value="save" />
-      </form>
-    )
+      <div className={style.container}>
+        <form onSubmit={handleSubmit} className={style.form}>
+          <input type='date' onChange={handleChange} required />
+          <input type='submit' className={style.btn} value='save' />
+        </form>
+      </div>
+    );
   },
   (prevProps, nextProps) => isEqual(prevProps, nextProps)
-)
+);
 
-export default CreateUserForm
+export default CreateUserForm;
