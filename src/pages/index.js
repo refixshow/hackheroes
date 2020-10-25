bb44647853e99834155b1cdabdaec0115ddcc93a
szymonhrehorowicz <szymhre@gmail.com>
2020-10-25 18:49:37 +0100

    nav

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
-export { default as Home } from "./home/Home"
-export { default as Error } from "./Error"
-export { default as Activities } from "./Activities"
-export { default as BMI } from "./BMI"
-export { default as Pressure } from "./Pressure"
-export { default as Covid19 } from "./Covid19"
-export { default as User } from "./User"
+export { default as Home } from "./Home/Home";
+export { default as Error } from "./Error";
+export { default as Activities } from "./Activities";
+export { default as BMI } from "./BMI";
+export { default as Pressure } from "./Pressure";
+export { default as Covid19 } from "./Covid19";
+export { default as User } from "./User";
