192a0b5fcd1787148b47253185f17e46bfe98c8a
szymonhrehorowicz <szymhre@gmail.com>
2020-10-25 16:20:10 +0100

    home scss

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,19 +1,24 @@
-import React, { useContext, useCallback } from "react"
-import { IdentityModalContext } from "../context/IdentityModalContextProvider"
+import React, { useContext, useCallback } from "react";
+import { IdentityModalContext } from "../context/IdentityModalContextProvider";
+import style from "./Home.module.scss";
+import logo from "../assets/icons/logo.svg";
 
 const Home = () => {
-  const { setIsOpen } = useContext(IdentityModalContext)
+  const { setIsOpen } = useContext(IdentityModalContext);
 
   const handleOnClick = useCallback(() => {
-    setIsOpen(true)
-  }, [setIsOpen])
+    setIsOpen(true);
+  }, [setIsOpen]);
 
   return (
-    <div>
-      Home Page
-      <button onClick={handleOnClick}>Log In</button>
-    </div>
-  )
-}
+    <main className={style.wrapper}>
+      <div className={style.content}>
+        <img src={logo} className={style.logo} />
+        <h1 className={style.header}>Health Companion</h1>
+        <button onClick={handleOnClick}>Log In</button>
+      </div>
+    </main>
+  );
+};
 
-export default Home
+export default Home;
