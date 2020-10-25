d04175529976d8c2250e6d5d32fb864539e67d0f
szymonhrehorowicz <szymhre@gmail.com>
2020-10-25 21:11:09 +0100

    addActivity css

--- src/pages/Activities.js
+++ src/pages/Activities.js
@@ -1,26 +1,24 @@
-import React, { useState } from "react"
-import ReactChart from "../components/organisms/ReactChart"
-import ChartWrapper from "../components/atoms/chartWrapper/ChartWrapper"
-import AddActivity from "../components/molecules/addActivity/AddActivity"
+import React, { useState } from "react";
+import ReactChart from "../components/organisms/ReactChart";
+import ChartWrapper from "../components/atoms/chartWrapper/ChartWrapper";
+import AddActivity from "../components/molecules/addActivity/AddActivity";
 
 const Activities = () => {
   const [active, setActive] = useState({
     chart: true,
     history: false,
     add: false,
-  })
+  });
 
   return (
     <main>
       <ChartWrapper>
-        {active.chart && (
-          <ReactChart queryKey="activities" endPointName="activity" />
-        )}
+        {active.chart && <ReactChart queryKey='activities' endPointName='activity' />}
         {active.history && "history"}
         {active.add && <AddActivity />}
       </ChartWrapper>
     </main>
-  )
-}
+  );
+};
 
-export default Activities
+export default Activities;
