8babb4392e71ffc60bb2764acac3a3f9fe4bedd5
szymonhrehorowicz <szymhre@gmail.com>
2020-10-25 18:04:03 +0100

    nav sass

--- src/components/atoms/navLink/NavLink.js
+++ src/components/atoms/navLink/NavLink.js
@@ -1,15 +1,19 @@
-import React from "react"
-import { Link, useLocation } from "react-router-dom"
-import style from "./NavLink.module.scss"
+import React from "react";
+import { Link, useLocation } from "react-router-dom";
+import Icon from "../icon/Icon";
+import style from "./NavLink.module.scss";
 
-const RouterLink = ({ to, children }) => {
-  const { pathname } = useLocation()
+const RouterLink = ({ to, children, icon }) => {
+  const { pathname } = useLocation();
 
   return (
-    <Link className={pathname === to ? style.active : ""} to={to}>
-      {children}
-    </Link>
-  )
-}
+    <div className={`${style.btn} ${pathname === to ? style.active : ""}`}>
+      <Link to={to}>
+        <Icon src={icon} className={style.icon} />
+      </Link>
+      {pathname === to ? <p className={style.desc}>{children}</p> : null}
+    </div>
+  );
+};
 
-export default RouterLink
+export default RouterLink;
--- src/components/molecules/nav/Nav.js
+++ src/components/molecules/nav/Nav.js
@@ -1,28 +1,39 @@
-import React from "react"
-import { useLocation } from "react-router-dom"
-import style from "./Nav.module.scss"
-import NavLink from "../../atoms/navLink/NavLink"
-
-// import { isLoggedIn } from "react-netlify-identity-widget"
+import React from "react";
+import { useLocation } from "react-router-dom";
+import style from "./Nav.module.scss";
+import NavLink from "../../atoms/navLink/NavLink";
+import { queryCache } from "react-query";
+import pressure_icon from "../../../assets/icons/activity.svg";
+import bmi_icon from "../../../assets/icons/check.svg";
+import activity_icon from "../../../assets/icons/dumbbell.svg";
+import settings_icon from "../../../assets/icons/menu.svg";
 
 const Nav = () => {
-  const location = useLocation()
+  const location = useLocation();
+
+  const user = queryCache.getQueryData("user");
 
   return (
-    <nav
-      className={
-        location.pathname === "/" || location.pathname === "/user" || false
-          ? style.navigationNone
-          : style.navigation
-      }
-    >
-      <NavLink to="/activities">activities</NavLink>
-      <NavLink to="/bmi">bmi</NavLink>
-      <NavLink to="/pressure">pressure</NavLink>
-      <NavLink to="/covid19">Covid</NavLink>
-      <NavLink to="/user">user</NavLink>
+    <nav className={location.pathname === "/" || location.pathname === "/user" ? style.navigationNone : style.navigation}>
+      <NavLink to='/pressure' icon={pressure_icon}>
+        pressure
+      </NavLink>
+      <br></br>
+      <NavLink to='/bmi' icon={bmi_icon}>
+        bmi
+      </NavLink>
+      <br></br>
+      <NavLink to='/activities' icon={activity_icon}>
+        activities
+      </NavLink>
+      <br></br>
+      {/* <NavLink to='/covid19'>Covid</NavLink>
+      <br></br> */}
+      <NavLink to='/user' icon={settings_icon}>
+        user
+      </NavLink>
     </nav>
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;
