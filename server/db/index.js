f9ef43658e91e27c69f61a493c6cd40afd867159
refixshow <adasscieszka@wp.pl>
2020-10-25 13:06:06 +0100

    deploy refactor

--- server/db/index.js
+++ server/db/index.js
@@ -1,19 +1,24 @@
 // Index.js for DB
 // Checks if database connection is already established
 // IF NOT connects to database
-const mongoose = require("mongoose");
+const mongoose = require("mongoose")
 
-let cachedDB = null;
-const uri = process.env.DB_PATH;
+let cachedDB = null
+const uri = process.env.DB_PATH
 
 const getConnection = async () => {
   if (!cachedDB) {
     cachedDB = await mongoose.connect(uri, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-    });
+    })
   }
-  return cachedDB;
-};
 
-module.exports = getConnection;
+  console.log(
+    cachedDB,
+    "IS CONNECTING -----------------------------------------"
+  )
+  return cachedDB
+}
+
+module.exports = getConnection
