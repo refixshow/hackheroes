8d8ed41e4b2b19dce766ea750bc850aa1e886ae7
refixshow <adasscieszka@wp.pl>
2020-10-25 20:46:05 +0100

    addBmi

--- src/pages/Pressure.js
+++ src/pages/Pressure.js
@@ -1,13 +1,25 @@
-import React from "react"
+import React, { useState } from "react"
 
 import ReactChart from "../components/organisms/ReactChart"
 import ChartWrapper from "../components/atoms/chartWrapper/ChartWrapper"
 
+import AddPressure from "../components/molecules/addPressure/AddPressure"
+
 const Pressure = () => {
+  const [active, setActive] = useState({
+    chart: true,
+    history: false,
+    add: false,
+  })
+
   return (
     <main>
       <ChartWrapper>
-        <ReactChart queryKey="pressure" endPointName="pressure" />
+        {active.chart && (
+          <ReactChart queryKey="pressure" endPointName="pressure" />
+        )}
+        {active.history && "history"}
+        {active.add && <AddPressure />}
       </ChartWrapper>
     </main>
   )
